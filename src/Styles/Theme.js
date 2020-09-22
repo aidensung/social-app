68c976e2174d4fe994ce44f10a4c24f7f90e9c17
Ilhyo Sung <sih8189@gmail.com>
2020-09-22 16:45:36 -0400

    initialized app and reset globalStyles and applied Theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = '1px solid #e6e6e6';
+const BORDER_RADIUS = '4px';
+
+export default {
+  bgColor: '#FAFAFA',
+  blackColor: '#262626',
+  darkGreyColor: '#989898',
+  lightGreyColor: '#c7c7c7',
+  blueColor: '#0195F6',
+  redColor: '#ED4956',
+  darkBlueColor: '#164776',
+  boxBorder: '1px solid #e6e6e6',
+  borderRadius: '',
+  whiteBox: ` ${BOX_BORDER};  
+              ${BORDER_RADIUS};
+              background-color: white;
+            `,
+};
